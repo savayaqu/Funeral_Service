9fc304a315d4dfb3820acc9243ff887be83e5998
savayaqu <zcomn@mail.ru>
2024-04-22 13:04:01 +0000

    Обновлено

--- app/Http/Controllers/ShiftController.php
+++ app/Http/Controllers/ShiftController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\ApiException;
-use App\Http\Requests\AddUserShiftRequest;
+use App\Http\Requests\ShiftRequest;
 use App\Models\Shift;
 
 use App\Models\User;
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 
 class ShiftController extends Controller
 {
-    public function addUser(AddUserShiftRequest $request, int $id) {
+    public function addUser(ShiftRequest $request, int $id) {
         $shift = Shift::where('id', $id)->first();
         if(!$shift) {
             throw new ApiException(404, 'Смена не найдена');
@@ -23,5 +23,17 @@ class ShiftController extends Controller
         $user->fill($request->all());
         $user->save();
         return response()->json(['message'=> 'Пользователь '.$request->input('user_id').' добавлен на смену '.$id])->setStatusCode(200);
+    }
+    public function deleteUser(ShiftRequest $request, int $id) {
+        $shift = Shift::where('id', $id)->first();
+        if(!$shift) {
+            throw new ApiException(404, 'Смена не найдена');
+        }
+        $user = User::where('id', $request->input('user_id'))->first();
+        if (!$user) {
+            throw new ApiException(404, 'Пользователь не найден');
+        }
+        $user->delete();
+        return response()->json(['message'=> 'Пользователь '.$request->input('user_id').' удалён со смены '.$id])->setStatusCode(200);
     }
 }
